91633e3d12e14a6b2f1a4efbf2259c1372c7d197
FukushimaTakeshi <whippet.818@gmail.com>
2021-09-16 21:03:07 +0900

    ロールAPIの形式を変更

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -3,8 +3,9 @@ class RolesController < ApplicationController
 
   def index
     user_roles = UserRole.preload(role: :permissions).where(user: current_user)
-    names = user_roles.map { |user_role| user_role.role.permissions.map(&:name) }.flatten
+    role_names = user_roles.map { |user_role| user_role.role.name }
+    permission_names = user_roles.map { |user_role| user_role.role.permissions.map(&:name) }.flatten
 
-    render json: names
+    render json: { roles: role_names, permissions: permission_names }
   end
 end
