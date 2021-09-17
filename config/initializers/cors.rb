baed3c705005e56d45a2f33f5539448cc6ba6875
FukushimaTakeshi <whippet.818@gmail.com>
2021-09-17 22:29:34 +0900

    CORS 本番環境設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,15 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8000'
-
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    if Rails.env.production?
+      origins ENV['PRODUCTION_DOMAIN']
+      resource '*',
+               headers: :any,
+               methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    else
+      origins 'localhost:8000'
+      resource '*',
+               headers: :any,
+               methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
   end
 end
