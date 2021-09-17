0749ee9d28ab160245778e1a425bd64f49885d8f
FukushimaTakeshi <whippet.818@gmail.com>
2021-09-17 23:12:59 +0900

    本番環境設定

--- config/initializers/firebase_auth_initializer.rb
+++ config/initializers/firebase_auth_initializer.rb
@@ -1,4 +1,4 @@
 FirebaseIdToken.configure do |config|
-  config.redis = Redis.new
+  config.redis = Rails.env.production? ? Redis.new(url: ENV['REDIS_URL']) : Redis.new
   config.project_ids = ['dirt-awesome']
 end
