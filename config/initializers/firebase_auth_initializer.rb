51441bd49c8f7cf5c688fcb125ae6df06a54d91e
FukushimaTakeshi <whippet.818@gmail.com>
2024-10-30 23:39:45 +0900

    change redis-url env

--- config/initializers/firebase_auth_initializer.rb
+++ config/initializers/firebase_auth_initializer.rb
@@ -1,4 +1,4 @@
 FirebaseIdToken.configure do |config|
-  config.redis = Rails.env.production? ? Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }) : Redis.new
+  config.redis = Rails.env.production? ? Redis.new(url: ENV["REDIS_TLS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }) : Redis.new
   config.project_ids = ['dirt-awesome']
 end
